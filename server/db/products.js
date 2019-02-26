406a35f116746bd64b4b2eb699659f07ca7893e5
Sam Apter <sam@sapter.com>
2019-02-26 16:05:04 -0500

    add models, change db name in package.json

---
+++ server/db/products.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define({
+  varietal: {
+    type: Sequelize.ENUM,
+    values: ['Beef', 'Pork', 'Chicken', 'Lamb']
+  },
+  cut: {
+    type: Sequelize.STRING
+  },
+  quantity: {
+    type: Sequelize.SMALLINT,
+    validate: {
+      min: 0
+    }
+  },
+  weight: {
+    type: Sequelize.SMALLINT,
+    validate: {
+      min: 0
+    }
+  },
+  price: {
+    type: Sequelize.SMALLINT,
+    validate: {
+      min: 0
+    }
+  }
+})
